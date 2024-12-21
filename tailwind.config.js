406e4aad93d3a44fe02d915e3a67fb1d2d955ffc
Abdullah Al Maruf <marufoffice.67@gmail.com>
2024-12-21 16:30:32 +0600

    cmt

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//       content: ["./src/**/*.{html,js}"],
+//       theme: {
+//         extend: {},
+//       },
+//       plugins: [],
+// }
+// module.exports = {
+//       content: ["./src/**/*.{html,js}"],
+//       theme: {
+//         extend: {
+//           colors: {
+//             orange: {
+//               900: '#7C2D12', // Or whatever color you'd like
+//             }
+//           }
+//         }
+//       }
+//     }
+    
